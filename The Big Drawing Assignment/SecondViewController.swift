25ae8eefe7f3e8cbf7e28ec7276ee8157550c1ec
echtniet <davelaarprogramming@gmail.com>
2019-11-22 14:46:02 -0600

    Initial Commit

---
+++ The Big Drawing Assignment/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  The Big Drawing Assignment
+//
+//  Created by Student on 11/22/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
